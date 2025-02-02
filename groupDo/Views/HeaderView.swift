aced643c30cc09a8086ab78ff016e4c0b46fc927
Reuben Remy <reubenremy@gmail.com>
2025-02-02 10:58:12 -0500

    abstract header view

---
+++ groupDo/Views/HeaderView.swift
@@ -0,0 +1,33 @@
+//
+//  HeaderView.swift
+//  groupDo
+//
+//  Created by Reuben Remy on 2/2/25.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(Color.pink)
+                .rotationEffect(Angle.degrees(15))
+            VStack {
+                Text("groupDo")
+                    .font(.system(size: 50, weight: .bold, design: .monospaced))
+                    .foregroundColor(.white)
+            }
+            .padding(.top, 50)
+        }
+        .frame(
+            width: UIScreen.main.bounds.width * 3,
+            height: 300
+        )
+        .offset(x: 0, y: -100)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
--- groupDo/Views/LoginView.swift
+++ groupDo/Views/LoginView.swift
@@ -11,22 +11,7 @@ struct LoginView: View {
     var body: some View {
         VStack {
             // header
-            ZStack {
-                RoundedRectangle(cornerRadius: 0)
-                    .foregroundColor(Color.pink)
-                    .rotationEffect(Angle.degrees(15))
-                VStack {
-                    Text("groupDo")
-                        .font(.system(size: 50, weight: .bold, design: .monospaced))
-                        .foregroundColor(.white)
-                }
-                .padding(.top, 50)
-            }
-            .frame(
-                width: UIScreen.main.bounds.width * 3,
-                height: 300
-            )
-            .offset(x: 0, y: -100)
+            HeaderView()
             
             // login form
             
