726cd44ab2088172e433abd7a6ef552e1606a1d3
Reuben Remy <reubenremy@gmail.com>
2025-02-02 10:56:24 -0500

    implement header

--- groupDo/Views/LoginView.swift
+++ groupDo/Views/LoginView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // header
+            ZStack {
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundColor(Color.pink)
+                    .rotationEffect(Angle.degrees(15))
+                VStack {
+                    Text("groupDo")
+                        .font(.system(size: 50, weight: .bold, design: .monospaced))
+                        .foregroundColor(.white)
+                }
+                .padding(.top, 50)
+            }
+            .frame(
+                width: UIScreen.main.bounds.width * 3,
+                height: 300
+            )
+            .offset(x: 0, y: -100)
+            
+            // login form
+            
+            // create account
+            Spacer()
+        }
     }
 }
 
