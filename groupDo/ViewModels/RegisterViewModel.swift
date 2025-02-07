5acc3f8d12726879d26c039b4ce930a93516bee2
Reuben Remy <reubenremy@gmail.com>
2025-02-06 22:40:23 -0500

    implement register view model

--- groupDo/ViewModels/RegisterViewModel.swift
+++ groupDo/ViewModels/RegisterViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class RegisterViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func register() {
+        guard validate() else {
+            return
+        }
+        
+        print("registering user...")
+    }
+    
+    private func validate() -> Bool {
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+        !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard email.contains(("@")) && email.contains((".")) else {
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            return false
+        }
+        
+        return true
+    }
+}
